d072d5e92c3d23bdb0e22a845fefc4742e703cc1
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-12-26 19:22:20 +0300

    mentorify project initialized

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const PORT = process.env.PORT ?? 8800
+const app = express()
+const connection = require('./utils/db-connection')
+const dotenv = require("dotenv")
+const helmet = require("helmet")
+const morgan = require("morgan")
+
+dotenv.config()
+
+//middleware
+app.use(express.json()) //when we make post request it's gonna parse it.
+app.use(helmet())
+app.use(morgan("common"))
+
+
+
+//db connection
+connection()
+
+app.listen(PORT, ()=>{
+    console.log("Backend server is running!")
+})
