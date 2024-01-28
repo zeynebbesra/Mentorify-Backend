c61612b71f4ed580ee681f11850fcc9d1ff13080
zeynebbesra <394777@ogr.ktu.edu.tr>
2024-01-28 13:00:16 +0300

    mentor routes updated

--- routes/mentor.routes.js
+++ routes/mentor.routes.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require('express')
 const mentorController = require('../controllers/mentor.controller')
 const router = express.Router()
 
 router
-    .route('/register')
+    .route('/register/mentor')
     .post(mentorController.register)
 
 router
-    .route('/login')
+    .route('/login/mentor')
     .post(mentorController.login)
 
 module.exports = router
