1b8fa104b284dba160af1b90cdd1bc09759f70ad
ulasdemir472 <ulasdemir472@gmail.com>
2024-05-06 18:57:17 +0300

    bug fixed in mentee routes

--- routes/mentee.routes.js
+++ routes/mentee.routes.js
@@ -1,43 +1,37 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('../utils/passport')
-const menteeController = require('../controllers/mentee.controller')
+const express = require("express");
+const router = express.Router();
+const passport = require("../utils/passport");
+const menteeController = require("../controllers/mentee.controller");
 
-router
-    .route('/register')
-    .post(menteeController.register)
+router.route("/register").post(menteeController.register);
 
-router
-    .route('/login')
-    .post(menteeController.login)
+router.route("/login").post(menteeController.login);
 
 // Google ile giriş yapma yolu
-router.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 // Google callback yolu
-router.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login/mentee' }),
-  menteeController.googleLogin);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login/mentee" }),
+  menteeController.googleLogin
+);
+
+router.route("/").get(menteeController.getMentees);
+
+router.route("/:id").get(menteeController.getMentee);
 
 router
-  .route('/')
-  .get(menteeController.getMentees)
+  .route("/add-to-wishlist/:menteeId/:mentorId")
+  .put(menteeController.addToWishlist);
 
 router
-  .route('/:id')
-  .get(menteeController.getMentee)
+  .route("/remove-from-wishlist/:menteeId/:mentorId")
+  .delete(menteeController.removeFromWishlist);
 
-router
-  .route('/add-to-wishlist/:menteeId/:mentorId')
-  .put(menteeController.addToWishlist)
+router.route("/wishlist/:menteeId").get(menteeController.getWishlist);
 
-router
-  .route('/remove-from-wishlist/:menteeId/:mentorId')
-  .put(menteeController.addToWishlist)
-
-router
-  .route('/wishlist/:menteeId')
-  .get(menteeController.getWishlist)
-
-module.exports = router
+module.exports = router;
