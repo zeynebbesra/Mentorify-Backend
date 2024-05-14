27bc1a77e68f395da4df7efc728946e6f33a4904
zeynebbesra <394777@ogr.ktu.edu.tr>
2024-05-14 14:41:53 +0300

    mentor update method fixed

--- controllers/mentor.controller.js
+++ controllers/mentor.controller.js
@@ -151,77 +151,46 @@ const login = async (req, res, next) => {
 };
 
 //Update Mentor
-
 const updateMentor = async (req, res, next) => {
   try {
-    const mentorExist = await Mentor.findById(req.params.id);
-    console.log(req.body);
-    if (!mentorExist) {
+    const { id } = req.params;
+    const updatedData = req.body;
+
+    if (updatedData.password) {
+      updatedData.password = await passwordHelper.passwordToHash(updatedData.password);
+    }
+
+    const imagePath = uploadImage(req);
+    if (imagePath) {
+      updatedData.image = imagePath;
+    }
+
+    const updatedMentor = await Mentor.findByIdAndUpdate(
+      id,
+      updatedData,
+      { new: true }
+    );
+
+    if (!updatedMentor) {
       return next(new ApiError("Mentor not found.", httpStatus.NOT_FOUND));
     }
 
-    let newPassword;
-
-    if (req.body.password) {
-      newPassword = await passwordHelper.passwordToHash(req.body.password);
-
-      mentorExist.password = newPassword;
-      await mentorExist.save();
-
-      return ApiDataSuccess.send(
-        "Password changed successfully!",
-        httpStatus.CREATED,
-        res
-      );
-    }
-
-    const imagePath = uploadImage(req, mentorExist.image);
-
-    const updatedMentor = await Mentor.findByIdAndUpdate(
-      req.params.id,
-      {
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        password: newPassword,
-        desc: req.body.desc,
-        category: req.body.category,
-        interest: req.body.interest,
-        image: imagePath,
-        github: req.body.github,
-        linkedin: req.body.linkedIn,
-        price: req.body.price,
-      },
-      { new: true }
-    );
-    console.log(req.body);
-
-    if (!updatedMentor) {
-      return next(
-        new ApiError(
-          "Mentor could not be updated due to an unexpected condition.",
-          httpStatus.INTERNAL_SERVER_ERROR
-        )
-      );
-    }
-
     return ApiDataSuccess.send(
-      "Profile changed successfully!",
-      httpStatus.CREATED,
+      updatedData.password ? "Password changed successfully!" : "Profile updated successfully!",
+      httpStatus.OK, 
       res,
       updatedMentor
     );
   } catch (error) {
-    return next(
-      new ApiError(
-        "Something went wrong :(",
-        httpStatus.BAD_REQUEST,
-        error.message,
-        console.log("error", error)
-      )
-    );
+    return next(new ApiError(
+      error.message,
+      console.log("error", error.message),
+      "Something went wrong :(",
+      httpStatus.INTERNAL_SERVER_ERROR
+    ));
   }
 };
+
 
 const deleteMentor = async (req, res, next) => {
   try {
